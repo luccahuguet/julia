f323a3c8f89efaa90f5f2f53868d151407ea80d3
luccahuguet <luccahuguet@hotmail.com>
2021-01-07 21:52:10 -0300

    more comments

--- newtonSqrt.jl
+++ newtonSqrt.jl
@@ -5,7 +5,12 @@ small_error_margin = 10^(-5)
 function newton_sqrt(input, error_margin=small_error_margin, guess=input / 2) # a=input/2 is the default value of `a`
   # The infinity assignment guarantees that e will be bigger than error_margin
   e = Inf
+
+  # The loop will keep iterating until the error e is smaller than error_margin
   while e > error_margin
+
+    # if guess is bigger than the true sqrt, input/guess will be less than sqrt, 
+    # so their avg will get closer and closer to the sqrt of input 
     avg = (guess + input/guess)/2.0
     e = convert(Float64, abs(avg - guess))
     guess = avg
