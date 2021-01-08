202060d8106bdaeb3fabb89b847cbef3113b6fc1
luccahuguet <luccahuguet@hotmail.com>
2021-01-07 21:39:07 -0300

    comments and modularization

--- newtonSqrt.jl
+++ newtonSqrt.jl
@@ -1,4 +1,9 @@
-function newton_sqrt(input, error_margin=(10^(-10)), guess=input / 2) # a=input/2 is the default value of `a`
+# The following function iteratively calculates the square root of a number
+
+small_error_margin = 10^(-5)
+
+function newton_sqrt(input, error_margin=small_error_margin, guess=input / 2) # a=input/2 is the default value of `a`
+  # The infinity assignment guarantees that e will be bigger than error_margin
   e = Inf
   while e > error_margin
     avg = (guess + input/guess)/2.0
